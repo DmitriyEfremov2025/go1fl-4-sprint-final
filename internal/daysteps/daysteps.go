baa7a34ec50530ed0fc5d46f53e57f988335c823
Dmitriy Efremov <effremovds@yandex.ru>
2025-12-10 22:34:45 +0300

    Добавить метод DayActionInfo()

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -2,9 +2,13 @@ package daysteps
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -58,4 +62,42 @@ func parsePackage(data string) (int, time.Duration, error) {
 
 func DayActionInfo(data string, weight, height float64) string {
 	// TODO: реализовать функцию
+	// Алгоритм реализации функции:
+	// 1. Получить данные о количестве шагов и продолжительности прогулки с помощью функции parsePackage(). В случае возникновения ошибки вывести её на экран и вернуть пустую строку.
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		log.Println("error: ", err)
+		return ""
+	}
+
+	// 2. Проверить, чтобы количество шагов было больше 0. В противном случае вернуть пустую строку.
+
+	if steps <= 0 {
+		log.Println("error: steps must be greater than zero")
+		return ""
+	}
+
+	// 3. Вычислить дистанцию в метрах. Дистанция равна произведению количества шагов на длину шага. Константа stepLength (длина шага) уже определена в коде.
+
+	distanceMeter := float64(steps) * stepLength
+
+	// 4. Перевести дистанцию в километры, разделив её на число метров в километре (константа mInKm, определена в пакете).
+	distanceKm := distanceMeter / mInKm
+
+	// 5. Вычислить количество калорий, потраченных на прогулке. Функция для вычисления калорий WalkingSpentCalories() будет определена в пакете spentcalories, которую вы тоже реализуете.
+
+	if weight <= 0 || height <= 0 {
+		log.Println("error: weight and height must be greater than zero")
+		return ""
+	}
+
+	calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+	if err != nil {
+		log.Println("error: ", err)
+		return ""
+	}
+
+	// 6. Сформировать строку, которую будете возвращать, пример которой был представлен выше.
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distanceKm, calories)
 }
